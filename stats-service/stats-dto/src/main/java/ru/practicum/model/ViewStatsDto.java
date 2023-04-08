0f697c23466ba0fd2cbb84108b77b929dc871f74
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-04-08 18:16:46 +0300

    feat: добавлен Сервис статистики.

---
+++ stats-service/stats-dto/src/main/java/ru/practicum/model/ViewStatsDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@Builder
+public class ViewStatsDto {
+
+    String app;
+
+    String uri;
+
+    Long hits;
+}
