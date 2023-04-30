5462d94b2c1f5367009252e12738868702227a28
AleksandrIsa4 <isachenkov.am@yandex.ru>
2023-04-30 17:15:45 +0300

    feat: добавлен Дополнительная функциональность лайки/дизлайки, рейтинг мероприятий. Замечания Вер_2.01

--- ewm-service/src/main/java/ru/practicum/model/enumeration/Grade.java
+++ ewm-service/src/main/java/ru/practicum/model/enumeration/Grade.java
@@ -1,10 +1,8 @@
 package ru.practicum.model.enumeration;
 
 import lombok.*;
-import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @ToString
 public enum Grade {
@@ -12,5 +10,5 @@ public enum Grade {
     LIKE(1L),
     DISLIKE(-1L);
 
-    Long score;
+    private final Long score;
 }
